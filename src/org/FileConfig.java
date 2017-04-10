7df1c42f380fa633516c05415ab1c316d6b2bb6f
ashwanitanwar <ashwanitanwar333@gmail.com>
2017-04-10 18:51:11 +0530

    temp

--- src/org/FileConfig.java
+++ src/org/FileConfig.java
@@ -1,8 +1,9 @@
 package org;
 
 public interface FileConfig {
-	String PROJECT_PATH = "F:\\";
-	String PROJECT_NAME = "exam_timetable\\";
+	
+	String PROJECT_PATH = "C:\\User\\ashwani tanwar\\workspace\\";
+	String PROJECT_NAME = "Exam_TimeTable_Scheduler_bySlots_Web_Project\\";
 	String INPUT_FILES_PATH = PROJECT_PATH + PROJECT_NAME + "src\\data\\input\\";
 	String OUTPUT_FILES_PATH = PROJECT_PATH + PROJECT_NAME + "src\\data\\output\\";
 	String EXAM_FILE_NAME = "ExamData.xlsx";
